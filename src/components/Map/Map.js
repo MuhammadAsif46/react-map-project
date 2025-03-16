be70bd665d37e3e95a67f641f00494da27d03035
MuhammadAsif46 <asifahmed32c@gmail.com>
2025-03-16 19:31:26 +0500

    add multiple markers

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,9 +2,8 @@ import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import React from "react";
 
 const Map = () => {
-//   const api_Key = "AIzaSyCcDba7w8OQqgWBCmje7-0qDWJHESQDJmA";
+  //   const api_Key = "AIzaSyCcDba7w8OQqgWBCmje7-0qDWJHESQDJmA";
   const api_Key = process.env.API_KEY;
-
 
   const containerStyle = {
     width: "100%",
@@ -15,10 +14,55 @@ const Map = () => {
     lat: 24.8607,
     lng: 67.0011,
   };
-  const anotherMarker = {
-    lat: 24.8773,
-    lng: 67.1591,
-  };
+  //   const anotherMarker = {
+  //     lat: 24.8773,
+  //     lng: 67.1591,
+  //   };
+
+  const makers = [
+    {
+      name: "location1",
+      location: {
+        lat: 24.8253,
+        lng: 67.1261,
+      },
+    },
+    {
+      name: "location2",
+      location: {
+        lat: 24.8607,
+        lng: 67.0011,
+      },
+    },
+    {
+      name: "location3",
+      location: {
+        lat: 24.8433,
+        lng: 67.0542,
+      },
+    },
+    {
+      name: "location4",
+      location: {
+        lat: 24.8602,
+        lng: 66.8637,
+      },
+    },
+    {
+      name: "location5",
+      location: {
+        lat: 24.9172,
+        lng: 67.0924,
+      },
+    },
+    {
+      name: "location6",
+      location: {
+        lat: 24.8773,
+        lng: 67.1591,
+      },
+    },
+  ];
 
   const { isLoaded } = useJsApiLoader({
     id: api_Key,
@@ -32,8 +76,10 @@ const Map = () => {
       //   onLoad={onLoad}
       //   onUnmount={onUnmount}
     >
-      <Marker position={center} />
-      <Marker position={anotherMarker} />
+      {makers.map((item) => (
+        <Marker key={item.name} position={item.location} />
+      ))}
+      {/* <Marker position={anotherMarker} /> */}
     </GoogleMap>
   ) : (
     <></>
